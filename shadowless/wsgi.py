5db098c685492e79512423e73ced2cc312a497ee
Teo Klestrup Röijezon <teo@nullable.se>
2013-12-20 03:32:38 +0100

    Monkeypatched dj_static since it errors out sometimes when writing headers

--- shadowless/wsgi.py
+++ shadowless/wsgi.py
@@ -28,8 +28,9 @@ import form_designer.contrib.cms_plugins.form_designer_form.models
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
+from shadowless.slstatic import SLCling
 from dj_static import Cling
-application = Cling(get_wsgi_application())
+application = SLCling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
