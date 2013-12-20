ac014b869ba92c970a163812e8dc9ebf09388610
Teo Klestrup Röijezon <teo@nullable.se>
2013-12-20 05:00:09 +0100

    removed a debug print

--- shadowless/slstatic.py
+++ shadowless/slstatic.py
@@ -3,6 +3,5 @@ from dj_static import Cling
 class SLCling(Cling):
     def __call__(self, environ, start_response):
         def patched_start_response(status, headers, exc_info=None):
-            print headers
             return start_response(status, [(str(k), str(v)) for k,v in headers], exc_info)
         return super(SLCling, self).__call__(environ, patched_start_response)
