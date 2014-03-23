8376d53716a578f627be69db7ab98ab8c433f18f
Teo Klestrup Röijezon <teo@nullable.se>
2014-03-23 21:31:07 +0100

    removed stale dependencies

--- shadowless/wsgi.py
+++ shadowless/wsgi.py
@@ -21,9 +21,6 @@ import os
 # os.environ["DJANGO_SETTINGS_MODULE"] = "shadowless.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shadowless.settings")
 
-# Need to import this module before we start the application when using WSGI, or it craps out for some reason
-import form_designer.contrib.cms_plugins.form_designer_form.models
-
 from django.conf import settings
 if settings.NEW_RELIC_CONFIG_FILE:
 	import newrelic.agent
